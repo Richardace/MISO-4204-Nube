0f3bac74d07277b1cdb0409df1fd59586c90c795
Santiago Lozano <santiago@slr.local>
2023-04-15 18:23:25 -0500

    Added 7z and tar.gz compression

--- async_tasks/tasks.py
+++ async_tasks/tasks.py
@@ -1,6 +1,9 @@
+import os
+import tarfile
 from celery import shared_task
 import zipfile
 import pymysql
+import py7zr
 
 def returnConection():
     try:
@@ -13,13 +16,30 @@ def returnConection():
 def add_together(a: int, b: int) -> int:
     return a + b
 
+def compressAsZip(targetName,sourceDir):
+    with zipfile.ZipFile(targetName, "w") as zf:
+        zf.write(sourceDir)
+
+def compressAs7z(targetName,sourceDir):
+    with py7zr.SevenZipFile(targetName, 'w') as z:
+        z.writeall(sourceDir)
+
+def compressAsTarGZ(targetName,sourceDir):
+    with tarfile.open(targetName, "w:gz") as tar:
+        tar.add(sourceDir, arcname=os.path.basename(sourceDir))
 
 @shared_task(ignore_result=False,name="startConversion")
 def startConversion(uid: str,fileName: str, newFormat: str) -> int:
 
     newFileName = "./processedFiles/"+uid+"."+newFormat
-    with zipfile.ZipFile(newFileName, "w") as zf:
-        zf.write("./uploads/"+fileName)
+    sourceDir = "./uploads/"+fileName
+    if newFormat == "zip":
+        compressAsZip(newFileName, sourceDir)
+    elif newFormat == "7z":
+        compressAs7z(newFileName, sourceDir)
+    elif newFormat == "tar.gz":
+        compressAs7z(newFileName, sourceDir)
+        
     conn = returnConection()
     with conn.cursor() as cur:
         sql = "UPDATE `dbconvert`.`archivos` SET `status` = 'PROCESSED', `processedFile`='{newFileName}' WHERE `fileIdentifier` = '{uid}'";
